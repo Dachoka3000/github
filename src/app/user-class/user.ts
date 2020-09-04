089490bdff52fdaacd2f56bbeb48df0d3f1450b2
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-04 08:22:10 +0300

    add a constructor for user class

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    constructor(
+        public name:string,
+        public avatar_url:any,
+        public public_repos:any,
+        public login: string,
+        public html_url:any,
+        public followers:any,
+        public following: any,
+        public repos_url:any,
+         ){}
 }
